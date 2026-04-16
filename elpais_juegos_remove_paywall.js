a3cfc556b40361cb8e870e07a2d84cc241b7677a
Marc Ribera <49065067+mriberadev@users.noreply.github.com>
2026-04-17 00:49:59 +0200

    Create elpais_juegos_remove_paywall.js
    Script removes paywall from El Pais' website and loads their games.

---
+++ elpais_juegos_remove_paywall.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Fuck Elpais
+// @namespace    http://tampermonkey.net/
+// @version      2026-04-16
+// @description  try to take over the world!
+// @author       You
+// @match        https://elpais.com/juegos/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=elpais.com
+// @grant        none
+// @run-at document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let retries = 50;
+
+    const intervalID = setInterval(_ => {
+        const match = document.querySelector("#ctn_freemium_article");
+        if(match) {
+            window.PMCompassHidePaywallLayer();
+            var elem = document.createElement("script");
+            elem.src = "https://cdn-elpais.smartgames.media/script.js?v="+Math.random().toString(36).substring(2, 10);
+            document.body.appendChild(elem);
+        };
+
+        retries--;
+        if(retries == 0 || match) clearInterval(intervalID);
+    }, 100);;
+})();
